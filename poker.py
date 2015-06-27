3733e933fc871d454484465f9898b3d4e606563b
Shane Mendez <shanemendez223@gmail.com>
2015-06-27 10:08:46 -0500

    can now find straights

--- poker.py
+++ poker.py
@@ -32,49 +32,85 @@ def deal():
 
 def find(hand,board):
     score = 0
-    incr = 0
-    m = 1
     count = 1
     maxcount = 0
+
     best= []
     collection = hand+board
     collection.sort()
     collection = collection[::-1]
+    name = "" #collection name
+
     output = ""
     #find a pair
     for i in range(0,len(collection)):
-        
+        temp_score = 0 # used to weigh hand scores against each other check_score
+        temp_name = name
         if i < len(collection)-1 and collection[i][0] == collection[i+1][0]: #if the ranks are the same
             count += 1 
-        elif count > 1:
-            best.append(collection[i-count+1:i+1])
+        #only true if next card is not the same. count tells the number of consecutive cards
+        elif count > 1: 
+
             output += "{0} match found {1}".format(count,collection[i-count+1:i+1])+"\n"
-            c_score = 0 # used to weigh hand scores against each other check_score
             
-            if count == maxcount == 2: #2 pair found
-                b = float(best[0][0][0])
-                c_score = 10**math.floor(math.log(b,10)+1)*((b%10)+1*(math.floor(math.log(b,10)))) 
-                c_score += best[1][1][0]
-                if c_score > score:
-                    score = c_score
             
-                    output += "2 pair found    {0}\n".format(score)
-            elif maxcount == 0: # first pair found
-                score = best[0][0][0] + 11
-                output += "pair found    {0}\n".format(score)
-            if count + maxcount == 5: output += "boat found\n"
+            if score < 13: # first pair found
+                temp_score = collection[i-count+1][0] + 11
+                #output += "pair found  {0}\n".format(collection[i-count+1:i+1])
+                temp_name = "pair"
+            
+            elif score < 32: #2 pair found
+                b = float(collection[i-count+1][0])
+                # 2pair scoring algorithm
+                temp_score = 10**math.floor(math.log(b,10)+1)*((b%10)+1*(math.floor(math.log(b,10)))) 
+                temp_score += best[0][0][0] #get Rank of pair in best
+                # used to be sure score for 2 pair is lower than previous score (if already scored)
+                temp_name = "2 pair"
+
+            
+
+            if count + maxcount == 5: 
+                name = "boat"
+                best.append(collection[i-count+1:i+1])
+            
             elif count == 3: 
-                score = best[-1][0][0] + 512
-                output += "found trips    {0}\n".format(score)
-            elif count == 4: output += "found quads\n"
+                temp_score = collection[i-count+1][0] + 512
+                temp_name = "trips"
+
+            elif count == 4: 
+                temp_name = "quads"
 
             if maxcount < count: maxcount = count
+
+            if temp_score > score:
+                score = temp_score
+                name = temp_name
+                best.append(collection[i-count+1:i+1])
             count = 1
         # output += str(count)+"\n"
 
         # output += "incr={0}".format(incr)+"\n"
 
+    #the second dict in s_check will contain an array of indexes of rank repetitions
+    s_check = [{},{}] # [{Rank: index},{Rank: [other indexes]}]
+    f_check = []
+    for i in range(0,len(collection)):
+        if collection[i][0] in s_check[0].keys():
+            if collection[i][0] not in s_check[1].keys():
+                s_check[1][collection[i][0]] = []
+            s_check[1][collection[i][0]].append(i)
+            continue
+        s_check[0][collection[i][0]] = i
 
+    if len(s_check[0]) >= 5:
+        for i in range(0,len(s_check[0])-4):
+            s = s_check[0].keys()
+            s.sort()
+            s = s[i:i+5]
+            if s[4]-4 == s[0]:
+                output += "straight found  {}".format(s)
+    output += "found {}    {}\n".format(name,best)
+    output += "score: {}\n".format(float(score))
     return output
 
 ## test vars/funcs
@@ -89,7 +125,7 @@ def convert(*cards):
 
 
 def test(runs):
-    f = open('pokertest.txt','w')
+    f = open('./testfind.txt','w')
     for _ in range(runs):
         redraw()
         shuffle(drawn)
