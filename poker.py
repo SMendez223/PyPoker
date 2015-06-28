64b9247abe40e5bc8f09e37c9bb3d550ce441d66
Shane Mendez <shanemendez223@gmail.com>
2015-06-28 08:57:46 -0500

    all hands scored, recognized, implemented Card and hand Classes

--- poker.py
+++ poker.py
@@ -96,55 +96,82 @@ def find(hand,board):
 
     best= []
     collection = hand+board
-    hand.sort_rank()
-    collection.sort_rank()
-
     name = "High card" #collection name
     score = 0
     count = 1
     maxcount = 0
     output = ""
     output +=  str(collection)+"\n"
+
+    temp_best = []
     #find a pair
     temp_score = 0L # used to weigh hand scores against each other check_score
     temp_name = name
+    
+    if score < 527:
+        #flip sort to high->low
+        # if the highest card in the straights rank -4 == the lowest rank its a straight
+        # ex: [10 J Q K A] -> flip -> [A K Q J 10] -> s[0] - 4 == s[4] == 10 -> STRAIGHT
+        # ex: [9 J Q K A] -> flip -> [A K Q J 9] -> s[0] - 4 != s[4]         -> NOT STRAIGHT :'(
+        s = is_straight(collection)
+        if s:
+            temp_name = "straight {} -> {}".format(s[-1].getrank(),s[0].getrank())
+            temp_score = long(526+s[4].getrankval())
+        if temp_score > score:
+            score = temp_score
+            name = temp_name
+            best = copy.deepcopy(s)
+            best = best[::-1] #flip it to make it low -> high
     for i in range(0,len(collection)):
+        
         if i < len(collection)-1 and collection[i][0] == collection[i+1][0]: #if the ranks are the same
             count += 1 
         #only true if next card is not the same. count tells the number of consecutive cards
         elif count > 1: 
-
+            temp_best = collection[i-count+1:i+1]
             output += "{0} match found {1}".format(count,collection[i-count+1:i+1])+"\n"
             
             
             if score < 13: # first pair found
-                temp_score = long(collection[i-count+1].getrankval() + 11)
+                temp_score = long(temp_best[0].getrankval() + 11)
                 #output += "pair found  {0}\n".format(collection[i-count+1:i+1])
-                temp_name = "pair {}'s".format(collection[i-count+1].getrank())
+                temp_name = "pair {}'s".format(temp_best[0].getrank())
             
             elif score < 32: #2 pair found
                 b = best[0].getrankval()
                 # 2pair scoring algorithm
                 temp_score = long(10**math.floor(math.log(b,10)+1)*((b%10)+1*(math.floor(math.log(b,10)))))
-                temp_score += collection[i-count+1].getrankval() #get Rank of pair in best
+                temp_score += temp_best[0].getrankval() #get Rank of pair in best
                 # used to be sure score for 2 pair is lower than previous score (if already scored)
-                temp_name = "2 "+temp_name+" {}'s".format(collection[i-count+1].getrank())
+                temp_name = "2 "+temp_name+" {}'s".format(temp_best[0].getrank())
 
             
 
-            if count + maxcount == 5: 
-                temp_name = "boat"
-                temp_score = long(collection[i-count+1].getrankval() + 512*2)
+
+            if count == 3: 
+                temp_score = long(temp_best[0].getrankval() + 512)
+                temp_name = "trip {}'s".format(temp_best[0].getrank())
                 if(len(best) == 4):
                     best = best[0:2]
-                
-            elif count == 3: 
-                temp_score = long(collection[i-count+1].getrankval() + 512)
+            if count + maxcount == 5: 
+                temp_name = "{}'s full of {}"
+                temp_score -= 512 # rank of trips
+                temp_score *= 1000
+                if count == 2:
+                    temp_name = temp_name.format(best[0].getrank(), temp_best[0].getrank())
+                    temp_score += temp_best[0].getrankval()
+                if count == 3:
+                    temp_name = temp_name.format(temp_best[0].getrank(), best[0].getrank())
+                    temp_score += best[0].getrankval()
+                temp_score += 200000000 # highest flush is 141,312,119
 
-                temp_name = "trips"
+                
+                
+            
 
             elif count == 4: 
-                temp_name = "quads"
+                temp_name = "quad {}'s".format(temp_best[0].getrank())
+                temp_score = temp_best[0].getrankval() + 300000000
 
             if maxcount < count: maxcount = count
 
@@ -157,57 +184,62 @@ def find(hand,board):
 
         # output += "incr={0}".format(incr)+"\n"
 
-    if score < 527:
-            #flip sort to high->low
-            # if the highest card in the straights rank -4 == the lowest rank its a straight
-            # ex: [10 J Q K A] -> flip -> [A K Q J 10] -> s[0] - 4 == s[4] == 10 -> STRAIGHT
-            # ex: [9 J Q K A] -> flip -> [A K Q J 9] -> s[0] - 4 != s[4]         -> NOT STRAIGHT :'(
-            s = is_straight(collection)
-            if s:
-                temp_name = "straight {} -> {}".format(s[-1].getrank(),s[0].getrank())
-                temp_score = long(526+s[4].getrankval())
-            if temp_score > score:
-                score = temp_score
-                name = temp_name
-                best = copy.deepcopy(s)
-                best = best[::-1] #flip it to make it low -> high
-    # ## FIND FLUSHES
-    # for key in f_check:
-    #     if len(f_check[key]) >= 5:
-    #         temp_name = "flush"
-    #         for i in range(0,len(f_check[key])-4):
-    #             temp_score = ""
-                
-    #             for card in f_check[key][i:i+5]:
-    #                 #digit sum the Ranks
-    #                 temp_score += str(get_rank(card))
-    #             temp_score = long(temp_score)
-    #             s = is_straight(f_check[key][i:i+5])
-    #             if s:
-    #                 temp_name = "straight flush"
-    #                 temp_score *= 100
-    #                 print "{} {}".format(f_check[key][0],f_check[key][0][0] == 14)
-    #                 if s[0][0] == 14:
-    #                     temp_name =  "royal flush"
-    #                     # temp_score = 2**80
 
-    #             if temp_score > score:
-    #                 score = temp_score
-    #                 name = temp_name
-    #                 best = f_check[key][i:i+5]
-        
+    # FIND FLUSHES
+    collection.sort_suit()
+    count = 0
+    for i in range(len(collection)-4):
+        if collection[i-count].getsuit() == collection[i+4].getsuit(): 
+            f_check = collection[i-count:i+5]
+            count += 1
+            temp_name = "flush"
+            temp_score = ""
+   
+    if f_check:
+        s = is_straight(Hand(f_check))
+        if s:
+            temp_name = "straight flush"
+            temp_score = "1000"
+            f_check = copy.deepcopy(s)
+            if s[0].getrankval() == 14:
+                temp_name = "Royal Flush"
+
+        for card in f_check:
+            temp_score += str(card.getrankval())
+        temp_score = long(temp_score)
+        if temp_score > score:
+            score = temp_score
+            name = temp_name
+            best = f_check  
+
     
+
+    ##find highest 5 card hand
     for card in collection:
         if card not in best and len(best) < 5:
             best.append(card)
-    if score == 0: score = best[0].getrankval()
-    output += "found {}    {}\n{}\n".format(name,best,score)
 
-    output += "score: {}\n".format(float(score))
+    # #if your highest 5 card hand uses your hand
+    # if set(hand) < set(best):
+    #     for card in hand:
+    #             score = 
+
+    # ## if all the best cards are on the board    
+    # if set(best) < set(board.cards()):
+    #     score = hand.cards[0].getrankval()
+    #     name = " {} Kicker".format(hand.cards[0].getrank())
+
+
+
+    output += "found {}    {}\n{}\n".format(name,best,score)
+    output += "score: {}\n".format(long(score))
     output += "\n\n"
     if name:
         return output,score
     return ""
+
+
+
 def is_straight(hand):
     h = {}
     assert type(hand) == Hand
@@ -249,7 +281,13 @@ def convert(*card):
     return cards
 
 
-def test(runs):
+def test():
+    f = open('./testfind.txt','w')
+    for i in range(len(t_hands)):
+        f.write(find(Hand(t_hands[i]),Hand(t_boards[i]))[0])
+        if not i%1000: print i
+    f.close()
+def testrand(runs):
     f = open('./testfind.txt','w')
     for i in range(runs):
         new_deck()
@@ -257,15 +295,15 @@ def test(runs):
         shuffle(deck)
         find_test = sorted(draw(7),key=lambda card: card[0])
         f.write(find(Hand(find_test[:2]),Hand(find_test[2:]))[0])
-        # if not i%1000: print i
+        if not i%1000: print i
     f.close()
 
-t_hands = convert(*"A,♦ 2,♣ 3,♥ 4,♦ 5,♦ 6,♣ 8,♥".split(" "))
-t_boards = "12H 11H 14D 14S 14C".split(" ")
+t_hands = [convert(*"A,♦ 10,♦".split(" "))]
+t_boards = [convert(*"J,♦ Q,♦ K,♦ 9,♦ 8,♦".split(" "))]
 # print find(t_hands,t_boards)
 drawn = []
 
-test(100)
+testrand(100)
 
 
 
